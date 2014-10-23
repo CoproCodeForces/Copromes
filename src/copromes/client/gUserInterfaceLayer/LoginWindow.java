0c8764355852871012dfa932791709bec313d1f8
Mitya <MityaWorker@gmail.com>
2014-10-23 19:24:20 +0400

    New look and feel for login window

--- src/copromes/client/gUserInterfaceLayer/LoginWindow.java
+++ src/copromes/client/gUserInterfaceLayer/LoginWindow.java
@@ -3,6 +3,7 @@ package copromes.client.gUserInterfaceLayer;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
@@ -42,47 +43,81 @@ public class LoginWindow extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-		JPanel backgroundPanel = new JPanel(new GridBagLayout());
-		backgroundPanel.setBackground(new Color(255, 255, 255));
-		backgroundPanel.setBorder(new EmptyBorder(new Insets(topPadding,
-				leftPadding, bottomPadding, rightPadding)));
-
+		JPanel panel = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.insets = new Insets(10, 80, 0, 10);
 		JLabel titleLabel = new JLabel("AUTHORIZATION");
-		backgroundPanel.add(titleLabel);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 0;
+		c.gridwidth = 2;
+//		titleLabel.setBorder(new EmptyBorder(0, 30, 0, 0));
+		panel.add(titleLabel, c);
 
-		JPanel loginPanel = new JPanel();
-		loginPanel.setBackground(getBackground());
+		c.insets = new Insets(10, 10, 0, 10);
+		
 		JLabel loginLabel = new JLabel("LOGIN");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 1;
+		c.weightx = 0;
+		c.gridwidth = 1;
 		loginLabel.setBorder(new EmptyBorder(0, 0, 0, 10));
-		JTextField loginInput = new JTextField(10);
-		loginInput.setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		loginPanel.add(loginLabel);
-		loginPanel.add(loginInput);
-		backgroundPanel.add(loginPanel);
+		panel.add(loginLabel, c);
 
-		JPanel passwordPanel = new JPanel();
-		passwordPanel.setBackground(getBackground());
+		JTextField loginInput = new JTextField(10);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 1;
+		c.weightx = 0;
+		c.gridwidth = 1;
+		loginInput.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(loginInput, c);
+
 		JLabel passwordLabel = new JLabel("PASSWORD");
 		passwordLabel.setBorder(new EmptyBorder(0, 0, 0, 10));
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 2;
+		c.weightx = 0;
+		c.gridwidth = 1;
+		panel.add(passwordLabel, c);
+
 		JPasswordField passwordInput = new JPasswordField(10);
 		passwordInput.setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		passwordPanel.add(passwordLabel);
-		passwordPanel.add(passwordInput);
-		backgroundPanel.add(passwordPanel);
-		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 2;
+		c.weightx = 0;
+		c.gridwidth = 1;
+		panel.add(passwordInput, c);
+
 		JButton loginButton = new JButton("DO LOGIN");
-		//loginButton.setPreferredSize(new Dimension(100, 20));
-		backgroundPanel.add(loginButton);
-		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 3;
+		c.weightx = 0;
+		c.gridwidth = 1;
+		panel.add(loginButton, c);
+
 		JButton registerButton = new JButton("FREE REGISTRATION");
-		//registerButton.setPreferredSize(new Dimension(25, 20));
-		backgroundPanel.add(registerButton);
-		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 4;
+		c.weightx = 0;
+		c.gridwidth = 2;
+		panel.add(registerButton, c);
+
 		JButton exitButton = new JButton("EXIT");
-		//exitButton.setPreferredSize(new Dimension(50, 20));
-		backgroundPanel.add(exitButton);
-		
-		add(backgroundPanel);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 5;
+		c.weightx = 0;
+		c.gridwidth = 2;
+		panel.add(exitButton, c);
+
+		add(panel);
 		pack();
 		setVisible(true);
 	}
