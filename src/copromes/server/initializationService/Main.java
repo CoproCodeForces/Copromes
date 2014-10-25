a08d14309bca1bbe53fc7e7945fd73ad053d281a
JohnDoeJr <coprocodeforces@gmail.com>
2014-10-25 18:24:38 +0400

    Added 'is client alive' check

--- src/copromes/server/initializationService/Main.java
+++ src/copromes/server/initializationService/Main.java
@@ -1,12 +1,22 @@
 package copromes.server.initializationService;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 import copromes.server.contactsService.*;
 import copromes.server.authorizationService.AuthorizationManager;
 import copromes.server.databaseService.DatabaseManager;
 import copromes.server.messengerService.MessengerManager;
 import copromes.server.networkService.Server;
+
+final class kek {
+	public List<String> l;
+	
+	public kek(List<String> t) {
+		l = t;
+	}
+}
 
 public class Main {
 
@@ -15,11 +25,12 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			Server server = new Server(port);
+			List<String> t = new ArrayList<String>();
 			
 			DatabaseManager dbManager = new DatabaseManager();			
 			AuthorizationManager authorizationManager = new AuthorizationManager(server, dbManager);			
 			ContactsManager contactsManager = new ContactsManager(server, dbManager);			
-			MessengerManager messengerManager = new MessengerManager(server, dbManager);			
+			MessengerManager messengerManager = new MessengerManager(server, dbManager);						
 			
 		} catch (RemoteException e) {
 
