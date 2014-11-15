7c69f2d4bf944d8ffb4254fb7c6b80585e2dac60
JohnDoeJr <coprocodeforces@gmail.com>
2014-11-15 13:09:29 +0400

    Added User parameter in sendMessage signature

--- src/copromes/commonInterfaces/IMessengerManager.java
+++ src/copromes/commonInterfaces/IMessengerManager.java
@@ -8,8 +8,8 @@ import copromes.domainLayer.Message;
 import copromes.domainLayer.User;
 
 public interface IMessengerManager extends Remote {
-	Message sendMessage(String message, ChatRoom chatRoom) throws RemoteException;
-	ChatRoom createChatRoom(User user, String name) throws RemoteException;
+	Message sendMessage(User author, String message, ChatRoom chatRoom) throws RemoteException;
+	ChatRoom createChatRoom(User author, String name) throws RemoteException;
 	ChatRoom addUserToChatRoom(User user, ChatRoom chatRoom) throws RemoteException;
 	ChatRoom deleteUserFromChatRoom(User user, ChatRoom chatRoom) throws RemoteException;
 	void deleteChatRoom(ChatRoom chatRoom) throws RemoteException;
