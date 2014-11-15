0ffaa539f4f47e96c2f85113424040bbe98977a5
JohnDoeJr <coprocodeforces@gmail.com>
2014-11-15 13:41:26 +0400

    Implemented server-side (not db!) message sending

--- src/copromes/server/messengerService/MessengerManager.java
+++ src/copromes/server/messengerService/MessengerManager.java
@@ -1,29 +1,41 @@
 package copromes.server.messengerService;
 
+import java.rmi.RemoteException;
+
 import copromes.commonInterfaces.IMessengerManager;
 import copromes.domainLayer.ChatRoom;
 import copromes.domainLayer.Message;
 import copromes.domainLayer.User;
 import copromes.server.databaseService.DatabaseManager;
+import copromes.server.networkService.Client;
 import copromes.server.networkService.Server;
 
 public class MessengerManager implements IMessengerManager {
-	
+
 	private Server server;
 	private DatabaseManager dbManager;
-	
+
 	public MessengerManager(Server server, DatabaseManager dbManager) {
 		super();
 		this.server = server;
 		this.server.setupMessengerManager(this);
-		
+
 		this.dbManager = dbManager;
 	}
 
 	@Override
-	public Message sendMessage(String message, ChatRoom chatRoom) {
+	public Message sendMessage(User author, String message, ChatRoom chatRoom) {
 		// TODO Auto-generated method stub
-		return null;
+		Message msg = dbManager.createMessage(author, message, chatRoom);
+		for (Client client : server.clients) {
+			try {
+				client.client.recieveMessage(author, msg);
+			} catch (RemoteException e) {
+				server.clients.remove(client);
+			}
+		}
+
+		return msg;
 	}
 
 	@Override
@@ -47,6 +59,6 @@ public class MessengerManager implements IMessengerManager {
 	@Override
 	public void deleteChatRoom(ChatRoom chatRoom) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
