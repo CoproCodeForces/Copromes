9c020612559a5e1add74ceba8bea80028a968c0f
JohnDoeJr <coprocodeforces@gmail.com>
2014-11-22 14:02:11 +0400

    Now server returns users online + removed bin FOLDER from repo

--- src/copromes/server/contactsService/ContactsManager.java
+++ src/copromes/server/contactsService/ContactsManager.java
@@ -1,10 +1,13 @@
 package copromes.server.contactsService;
 
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.List;
 
 import copromes.commonInterfaces.IContactsManager;
 import copromes.domainLayer.User;
 import copromes.server.databaseService.DatabaseManager;
+import copromes.server.networkService.Client;
 import copromes.server.networkService.Server;
 
 public class ContactsManager implements IContactsManager {
@@ -21,25 +24,28 @@ public class ContactsManager implements IContactsManager {
 	}
 	
 	@Override
-	public List<User> getUsersOnline() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<User> getUsersOnline() throws RemoteException {
+		List<User> users = new ArrayList<User>();
+		for (Client client : server.clients) {
+			users.add(client.user);
+		}
+		return users;
 	}
 
 	@Override
-	public void addFriend(User friend) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteFriend(User friend) {
+	public void addFriend(User friend) throws RemoteException {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public List<User> findUser(String name) {
+	public void deleteFriend(User friend) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<User> findUser(String name) throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
 	}
